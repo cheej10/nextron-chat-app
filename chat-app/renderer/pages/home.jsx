fc0e212131eff0a892c2f72246b07404545e81f9
heejung <cheej10@naver.com>
2022-08-09 15:20:54 +0900

    add user list

--- chat-app/renderer/pages/home.jsx
+++ chat-app/renderer/pages/home.jsx
@@ -1,78 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { auth } from '../firebase-config';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 function Home() {
-  const router = useRouter();
+  const [userList, setUserList] = useState([]);
 
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
+  const getUsers = () => {
+    const db = getDatabase();
+    const usersRef = ref(db, 'users/');
 
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
+    onValue(usersRef, (snapshot) => {
+      const data = snapshot.val();
+      setUserList(
+        Object.keys(data).map((key) => {
+          return {
+            key,
+            email: data[key].email,
+            nickname: data[key].nickname,
+          };
+        })
       );
-
-      router.push('/next');
-    } catch (error) {
-      alert('이메일이나 비밀번호가 잘못되었습니다.');
-    }
+    });
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+    });
+
+    getUsers();
+  }, []);
+
+  console.log(userList);
 
   return (
     <React.Fragment>
       <Head>
-        <title>Home</title>
+        <title>홈</title>
       </Head>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              이메일
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              onChange={(e) => setLoginEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              비밀번호
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              onChange={(e) => setLoginPassword(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={login}
-            >
-              로그인
-            </button>
-            <p className="my-2 cursor-pointer text-gray-700 underline text-sm">
-              <Link href="/signup">회원가입</Link>
-            </p>
-          </div>
-        </form>
+      <div className="w-1/3 bg-white rounded-lg shadow">
+        <ul className="divide-y-2 divide-gray-100">
+          {userList.map(({ key, nickname }) => (
+            <li key={key} className="p-3 text-gray-700">
+              {nickname}
+            </li>
+          ))}
+        </ul>
       </div>
     </React.Fragment>
   );
--- chat-app/renderer/pages/login.jsx
+++ chat-app/renderer/pages/login.jsx
@@ -1,42 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { auth } from '../firebase-config';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { getDatabase, set, ref } from 'firebase/database';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-function Signup() {
-  const [signupEmail, setSignupEmail] = useState('');
-  const [signupNickname, setSignupNickname] = useState('');
-  const [signupPassword, setSignupPassword] = useState('');
+function Login() {
+  const router = useRouter();
 
-  const signup = async () => {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const login = async () => {
     try {
-      const user = await createUserWithEmailAndPassword(
+      const user = await signInWithEmailAndPassword(
         auth,
-        signupEmail,
-        signupPassword
+        loginEmail,
+        loginPassword
       );
 
-      saveUserData(signupEmail, signupNickname, user.user.uid);
+      router.push('/home');
     } catch (error) {
-      console.log(error);
+      alert('이메일이나 비밀번호가 잘못되었습니다.');
     }
-  };
-
-  const saveUserData = (signupEmail, signupNickname, userId) => {
-    const db = getDatabase();
-
-    set(ref(db, 'users/' + userId), {
-      email: signupEmail,
-      nickname: signupNickname,
-    });
   };
 
   return (
     <React.Fragment>
       <Head>
-        <title>회원가입</title>
+        <title>로그인</title>
       </Head>
       <div className="w-screen h-screen flex items-center justify-center">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -51,21 +43,7 @@ function Signup() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
-              onChange={(e) => setSignupEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              닉네임
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="nickname"
-              type="text"
-              onChange={(e) => setSignupNickname(e.target.value)}
+              onChange={(e) => setLoginEmail(e.target.value)}
             />
           </div>
           <div className="mb-6">
@@ -79,19 +57,19 @@ function Signup() {
               className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              onChange={(e) => setSignupPassword(e.target.value)}
+              onChange={(e) => setLoginPassword(e.target.value)}
             />
           </div>
           <div className="flex flex-col items-center justify-center">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
-              onClick={signup}
+              onClick={login}
             >
-              회원가입
+              로그인
             </button>
             <p className="my-2 cursor-pointer text-gray-700 underline text-sm">
-              <Link href="/home">로그인</Link>
+              <Link href="/signup">회원가입</Link>
             </p>
           </div>
         </form>
@@ -100,4 +78,4 @@ function Signup() {
   );
 }
 
-export default Signup;
+export default Login;
